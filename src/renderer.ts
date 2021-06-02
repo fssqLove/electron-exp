39f9926af96b67601f3a517866ffc38c49254604
shenqi.lv <248120694@qq.com>
2021-06-02 16:43:25 +0800

    完成 electron react配置

--- src/renderer.ts
+++ src/renderer.ts
@@ -25,7 +25,7 @@
  *  });
  * ```
  */
-
+import './app.tsx';
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
--- webpack.rules.js
+++ webpack.rules.js
@@ -24,4 +24,14 @@ module.exports = [
       }
     }
   },
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        exclude: /node_modules/,
+        presets: ['@babel/preset-react']
+      }
+    }
+  },
 ];
