c82c3da053b214dd3d1275b0215f353090668f06
shenqi.lv <248120694@qq.com>
2021-06-06 19:16:29 +0800

    增加主进程与渲染进程通过ipc通讯

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 function createWindow() {
@@ -6,11 +6,14 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false
         }
     })
 
     win.loadFile('index.html')
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
@@ -23,4 +26,16 @@ app.whenReady().then(() => {
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
+})
+
+
+// 在主进程中.
+ipcMain.on('asynchronous-message', (event, arg) => {
+    console.log(arg) // prints "ping"
+    event.reply('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+    console.log(arg) // prints "ping"
+    event.returnValue = 'pong'
 })
--- renderer.js
+++ renderer.js
@@ -1,1 +1,10 @@
-console.log('hello')
+const { ipcRenderer } = require('electron')
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+    console.log(arg) // prints "pong"
+})
+ipcRenderer.send('asynchronous-message', 'ping')
+console.log('hello', ipcRenderer)
+
+
