6bc1ddc4285b0dcb621834d7f67edf695f98d4af
shenqi.lv <shenqi.lv@31huiyi.com>
2021-10-13 17:41:38 +0800

    增加 react-router-dom

--- src/renderer/about_window/index.tsx
+++ src/renderer/about_window/index.tsx
@@ -1,24 +1,61 @@
-import React, { useState } from 'react';
+import React, { FC } from 'react';
 import { render } from 'react-dom';
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom';
 
-import '../public/css/reset.css';
-import '../public/font/iconfont.css';
-import './index.scss';
-
-function App() {
-  const [state, setState] = useState('CLICK ME');
-
+function Home() {
   return (
     <div>
-      <div className="box">
-        <div className="content">
-          ，永不退缩
-          <i className="iconfont icon-xiazai" />
-        </div>
-      </div>
-      <button type="button" onClick={() => setState(state + 2)}>{state}</button>
+      <h3>Home Page</h3>
     </div>
   );
 }
+
+function User() {
+  return (
+    <div>
+      <h3>User Page</h3>
+    </div>
+  );
+}
+
+function Empty() {
+  return (
+    <div>
+      <h3>Empty Page</h3>
+    </div>
+  );
+}
+
+const App:FC = () => (
+  <div>
+    <h3>RouterPage</h3>
+
+    <Router>
+      <Link to="/">首页</Link>
+      <Link to="/user">用户中心</Link>
+
+      <Switch>
+        {/* exact 实现精确匹配 */}
+        <Route
+          exact
+          path="/"
+          component={Home}
+        />
+
+        <Route
+          path="/user"
+          component={User}
+        />
+
+        <Route
+          component={Empty}
+        />
+      </Switch>
+
+    </Router>
+  </div>
+);
 
 render(<App />, document.getElementById('root'));
